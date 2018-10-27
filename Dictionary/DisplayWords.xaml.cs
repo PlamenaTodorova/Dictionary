ea12cc79087812efe851ce0d5ee8ee88f617790c
Plamena Todorova <plamena.i.todorova@gmail.com>
2018-10-28 00:23:51 +0200

    Update DisplayWords.xaml.cs

--- Dictionary/DisplayWords.xaml.cs
+++ Dictionary/DisplayWords.xaml.cs
@@ -44,6 +44,37 @@ namespace Dictionary
             }
         }
 
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            string stringId =
+                ((TextBlock)
+                ((Grid)
+                ((Border)
+                (((StackPanel)
+                ((Button)sender).Parent).Parent)).Parent).Children[0]).Text;
+
+            int id = int.Parse(stringId);
+
+            WordBindingModel model = controller.GetWord(id);
+
+            EditWord window = new EditWord(model);
+
+            window.ShowDialog();
+
+            if (window.DialogResult == true)
+            {
+                model = window.newWord;
+
+                //Edit the word
+                controller.ChangeWord(id, model);
+            }
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         private void LanguageSelected(object sender, SelectionChangedEventArgs e)
         {
             if (pickLanguage.SelectedValue != null)
@@ -58,3 +89,4 @@ namespace Dictionary
         }
     }
 }
+
