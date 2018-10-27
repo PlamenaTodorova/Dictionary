f277aae8f6a378f935ee39b9f50988dbeae1dd41
Plamena Todorova <plamena.i.todorova@gmail.com>
2018-10-27 20:33:29 +0200

    Update DisplayWords.xaml.cs

--- Dictionary/DisplayWords.xaml.cs
+++ Dictionary/DisplayWords.xaml.cs
@@ -1,18 +1,7 @@
-﻿using DataStorage;
+using DataStorage;
 using Models.BindingModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Dictionary
 {
@@ -52,9 +41,6 @@ namespace Dictionary
 
                 //Add to the collection
                 controller.AddWord(model);
-
-                //Refresh the collection
-                words.ItemsSource = controller.GetWords();
             }
         }
 
@@ -72,4 +58,3 @@ namespace Dictionary
         }
     }
 }
-
