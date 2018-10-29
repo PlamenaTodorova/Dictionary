f00a98614c6b504309a7d1c2ea9e641be34dfdd2
Plamena Todorova <plamena.i.todorova@gmail.com>
2018-10-29 16:12:32 +0100

    Add files via upload

---
+++ Models/BindingModels/SearchBindingModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.BindingModels
+{
+    public class SearchBindingModel : INotifyPropertyChanged
+    {
+        private string toSearch;
+
+        public string ToSearch
+        {
+            get { return this.toSearch; }
+            set
+            {
+                if (this.toSearch != value)
+                {
+                    this.toSearch = value;
+                    this.NotifyPropertyChanged("ToSearch");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+}
