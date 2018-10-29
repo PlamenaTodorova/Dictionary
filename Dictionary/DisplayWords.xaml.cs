fad806ef9cddc330952eff61e85b6519a7f49ea6
Plamena Todorova <plamena.i.todorova@gmail.com>
2018-10-29 16:14:57 +0100

    Update DisplayWords.xaml.cs

--- Dictionary/DisplayWords.xaml.cs
+++ Dictionary/DisplayWords.xaml.cs
@@ -11,14 +11,17 @@ namespace Dictionary
     public partial class DisplayWords : Window
     {
         private DictionaryController controller;
+        private SearchBindingModel search;
 
         public DisplayWords(DictionaryController controller)
         {
             this.controller = controller;
+            this.search = new SearchBindingModel();
 
             InitializeComponent();
 
             pickLanguage.ItemsSource = controller.GetLanguages();
+            searchBar.DataContext = search;
             words.ItemsSource = controller.GetWords();
         }
 
@@ -42,6 +45,51 @@ namespace Dictionary
                 //Add to the collection
                 controller.AddWord(model);
             }
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            string stringId =
+                ((TextBlock)
+                ((Grid)
+                ((Border)
+                (((StackPanel)
+                ((Button)sender).Parent).Parent)).Parent).Children[0]).Text;
+
+            int id = int.Parse(stringId);
+
+            WordBindingModel model = controller.GetWord(id);
+
+            EditWord window = new EditWord(model);
+
+            window.ShowDialog();
+
+            if (window.DialogResult == true)
+            {
+                model = window.newWord;
+
+                //Edit the word
+                controller.ChangeWord(id, model);
+            }
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            string stringId =
+                ((TextBlock)
+                ((Grid)
+                ((Border)
+                (((StackPanel)
+                ((Button)sender).Parent).Parent)).Parent).Children[0]).Text;
+
+            int id = int.Parse(stringId);
+
+            controller.RemoveWord(id);
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            words.ItemsSource = controller.GetWords(search.ToSearch);
         }
 
         private void LanguageSelected(object sender, SelectionChangedEventArgs e)
